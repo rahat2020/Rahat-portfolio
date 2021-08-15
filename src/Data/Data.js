63bb08b8d9ff8de014559631c53b70d8377d724c
rahat2020 <bloodyhell4040@gmail.com>
2021-08-15 22:51:12 +0600

    code edited

---
+++ src/Data/Data.js
@@ -0,0 +1,51 @@
+const Data = [
+    {
+        name: "Barbar Shopping",
+        id: 1,
+        image: "../img/barberShop.png",
+        category: "fashion",
+        description: "In this website customer can book online for hair cut & styles booking service",
+    },
+
+    {
+        name: "RetroBlog",
+        id: 3,
+        image: "../img/retroBlog.png",
+        category: "news",
+        description: "In this website customers can order for the fruits  and it will deliver to the customer"
+
+    },
+    {
+        name: "Hungry naki",
+        id: 4,
+        image: "../img/hungry.png",
+        category: "food",
+        description: "In this website customers can order for the fruits  and it will deliver to the customer"
+
+    },
+    {
+        name: "fancy  slider",
+        id: 5,
+        image: "../img/fancySlider.png",
+        category: "pictures",
+        description: "In this website customers can order for the",
+    },
+    {
+        name:"BD Rail ticket",
+        id: 6,
+        image:"../img/rail.png",
+        category: "services",
+        description: "In this website customers can order"
+    },
+    {
+        name:"ClothStore",
+        id: 6,
+        image:"../img/Clothstore.png",
+        category: "services",
+        description: "In this website customers can order"
+    }
+
+
+
+]
+export default Data;
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,73 +1,64 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Projects.css';
 import Navbar from '../Navbar/Navbar';
-// import euro from '../../img/euro.png';
-import plumber from '../../img/plumber.png';
-// import barber from '../../img/barber.png';
-// import hungry from '../../img/hungry.png';
+import Data from '../../Data/Data';
+import { Link } from 'react-router-dom';
 const Projects = () => {
+    const [items, setItems] = useState(Data);
+
+    const filterItem = (categItem) => {
+        const updatedItems = Data.filter((curElem) => {
+            return curElem.category === categItem;
+        });
+
+        setItems(updatedItems);
+    }
     return (
         <section className="project container">
             <Navbar />
-            <div className="row card-style mt-4">
-                <div className="col-md-4">
-                    <div class="card">
-                        <img src={plumber} className="card-img-top image" alt="..." />
-                        <div className="card-body">
-                            <p>Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            <h5 className="text-center">Major Technologies</h5>
-                            <div className="technologies d-flex">
-                                <button class="btn-sizes">Node JS</button>
-                                <button class="btn-sizes">JavaScript</button>
-                                <button class="btn-sizes active">React JS</button>
-                                <button class="btn-sizes">Express JS</button>
-                                <button class="btn-sizes">Express JS</button>
-                            </div>
-                        </div>
-                        <div class="overlay">
-                            <button className="text btn btn-primary">View site</button>
-                        </div>
-                    </div>
-                </div>
-                <div className="col-md-4">
-                    <div class="card">
-                        <img src={plumber} className="card-img-top image" alt="..." />
-                        <div className="card-body">
-                            <p>Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            <h5 className="text-center">Major Technologies</h5>
-                            <div className="technologies d-flex">
-                                <button class="btn-sizes">Node JS</button>
-                                <button class="btn-sizes">JavaScript</button>
-                                <button class="btn-sizes active">React JS</button>
-                                <button class="btn-sizes">Express JS</button>
-                                <button class="btn-sizes">Express JS</button>
-                            </div>
-                        </div>
-                        <div class="overlay">
-                            <button className="text btn btn-primary">View site</button>
-                        </div>
-                    </div>
-                </div>
-                <div className="col-md-4">
-                    <div class="card">
-                        <img src={plumber} className="card-img-top image" alt="..." />
-                        <div className="card-body">
-                            <p>Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            <h5 className="text-center">Major Technologies</h5>
-                            <div className="technologies d-flex">
-                                <button class="btn-sizes">Node JS</button>
-                                <button class="btn-sizes">JavaScript</button>
-                                <button class="btn-sizes active">React JS</button>
-                                <button class="btn-sizes">Express JS</button>
-                                <button class="btn-sizes">Express JS</button>
-                            </div>
-                        </div>
-                        <div class="overlay">
-                            <button className="text btn btn-primary">View site</button>
-                        </div>
-                    </div>
-                </div>
+            <h1 className="mt-5 text-center main-heading">Here is my project</h1>
+            <hr />
 
+            <div className="menu-tabs">
+                <div className="d-flex">
+                    <button className="btn btn-warning" onClick={() => filterItem('fashion')}>Fashion</button>
+                    <button className="btn btn-warning" onClick={() => filterItem('food')}>Food</button>
+                    <button className="btn btn-warning" onClick={() => filterItem('services')}>Services</button>
+                    <button className="btn btn-warning" onClick={() => filterItem('pictures')}>Pictures</button>
+                    <button className="btn btn-warning" onClick={() => filterItem('news')}>News</button>
+                    <button className="btn btn-warning" onClick={() => setItems(Data)}>All</button>
+                </div>
+            </div>
+
+            {/* my main items section  */}
+            <div className="row mt-5">
+
+                {
+                    items.map((item) => (
+                            <div className="col-md-4">
+                                <div class="card">
+                                    <img src={item.image} className="card-img-top image" alt="..." />
+                                    <div className="card-body">
+                                        <p>{item.description}</p>
+                                        <h5 className="text-center">{item.name}</h5>
+                                        <h5 className="text-center">Major Technologies</h5>
+                                        <div className="technologies d-flex">
+                                            <button class="btn-sizes">Node JS</button>
+                                            <button class="btn-sizes">JavaScript</button>
+                                            <button class="btn-sizes active">React JS</button>
+                                            <button class="btn-sizes">Express JS</button>
+                                            <button class="btn-sizes">Express JS</button>
+                                        </div>
+                                    </div>
+                                    <div class="overlay">
+                                        <Link to="/">
+                                            <button className="text btn btn-primary">View site</button>
+                                        </Link>
+                                    </div>
+                                </div>
+                            </div>
+                    ))
+                }
             </div>
         </section>
     );
